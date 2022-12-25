85fc5a974eb2935bcb241cd4f30fd4b4d49e51ee
Ousmanou-Ardo <ousmanou@iut-dhaka.edu>
2022-12-25 09:44:41 -0500

    modified

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,10 +1,15 @@
-from functools import reduce
-
-def pascal_triangle(level, acc = None):
-    if not acc: acc = [[1]]
-    if level == 1: return acc
-    acc.append ( [1] + [a + b for a, b in zip (acc [-1], acc [-1] [1:] ) ] + [1] )
-    return pascal_triangle(level - 1, acc)
+def pascal_triangle(n): 
+    tree = [[1]] #first row is always just 1
+    for x in range(1, n): #for every other row:
+        if n <= 0:
+         return []
+        else:
+         m = [1] #it always starts with 1
+         for y in range(1, x): #for all the others:
+            m.append(tree[x-1][y-1] + tree[x-1][y]) #work out what the number is
+         m.append(1) #it always ends in 1
+         tree.append(m) #add the row on
+    return tree
 
                
                
