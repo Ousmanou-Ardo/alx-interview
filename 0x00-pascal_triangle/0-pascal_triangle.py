4e0f6c8ce277264d3267d47645045bc8df747c48
Ousmanou-Ardo <ousmanou@iut-dhaka.edu>
2022-12-25 10:00:59 -0500

    modi

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,15 +1,14 @@
-def pascal_triangle(n): 
+def pascal_triangle(n):
     if n <= 0:
-         tree = [[]]
-         return tree
-    else: tree = [[1]] #first row is always just 1
-    for x in range(1, n): #for every other row:
-         m = [1] #it always starts with 1
-         for y in range(1, x): #for all the others:
-            m.append(tree[x-1][y-1] + tree[x-1][y]) #work out what the number is
-         m.append(1) #it always ends in 1
-         tree.append(m) #add the row on
+        tree = [[]]
+        return tree
+    else:
+        tree = [[1]]  # first row is always just 1
+    for x in range(1, n):  # for every other row:
+        m = [1]  # it always starts with 1
+        for y in range(1, x):  # for all the others:
+            # work out what the number is
+            m.append(tree[x-1][y-1] + tree[x-1][y])
+        m.append(1)  # it always ends in 1
+        tree.append(m)  # add the row on
     return tree
-
-               
-               
